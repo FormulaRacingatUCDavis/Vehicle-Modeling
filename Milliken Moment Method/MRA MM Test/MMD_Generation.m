8adbff291e2fc972a179b83accf972a3c3fb8bb1
andywang2017 <rmiwang@ucdavis.edu>
2024-10-28 18:46:02 -0700

    Update MMD_Generation.m

--- Milliken Moment Method/MRA MM Test/MMD_Generation.m
+++ Milliken Moment Method/MRA MM Test/MMD_Generation.m
@@ -38,10 +38,11 @@ format long g
 % 1  2      Postive Y
 % 3  4
 % Backwards
+% RIGHT HAND TURN RESULTS IN POSTIVE AY
 
 g = 9.81; % Grav constant
-m = 100; %kg
-PFront = 52 /100;
+m = 250; %kg
+PFront = 60 /100;
 WB = 1.5; %Wheelbase - m
 TWf = 1.2; %Trackwidth - m
 TWr = 1.2;
@@ -59,9 +60,9 @@ Model = struct( 'Pure', 'Pacejka', 'Combined', 'MNC' );
 load('Hoosier_R25B_16x75-10x7.mat');
 
 % dSteer = deg2rad(linspace(-30,30,41))';
-SA_CG = deg2rad(linspace(-12,12,41))';
+SA_CG = deg2rad(linspace(-12,12,31))';
 
-dSteer = deg2rad(linspace(-15,15,41))';
+dSteer = deg2rad(linspace(-25,25,31))';
 % SA_CG = deg2rad(linspace(-9,9,41))';
 
 dSteer_W1 = toe_f + dSteer;
@@ -104,12 +105,14 @@ for i = 1:length(dSteer)
         AxGuess = 0;
         AyGuess = 0;
         res = 1;
-        resAx = 1;
         tol = 1e-3;
+
         itAyBody = [];
         itAyBody(1,1) = AyGuess;
         itAxBody = [];
         itAxBody(1,1) = AxGuess;
+        itCAyBody = [];
+        itCAyBody(1,1) = AyGuess/g;
         itFz = [];
 
         c = 1;
@@ -134,6 +137,8 @@ for i = 1:length(dSteer)
                 %                     / (V.* cos(SA_CG(j)) - Omega .* coord_AllW(2,p)) ) - dSteer_AllW(i,p);
                 % SA_Wheel(p,1) = atan( (R.*sin(SA_CG(j)) + coord_AllW(1,p)) / (R.*cos(SA_CG(j)) - coord_AllW(2,p)) )...
                 %                 - dSteer_AllW(i,p);
+
+                % Free Rolling MMD Assumption
                 SA_Wheel(p,1) = SA_CG(j) - dSteer_AllW(i,p);
             
                 V_Wheel(p,1) = V + sqrt(  (-Omega.*coord_AllW(2,p)).^2  +  (Omega.*coord_AllW(1,p)).^2  );
@@ -143,8 +148,8 @@ for i = 1:length(dSteer)
             V_Wheel = V_Wheel .* 0;
             
             dFzf_dAx = (hCG .* m)./(2.* WB);
-            dFzf_dAy = .5*(hCG .* m .* g .* PFront)/TWf;
-            dFzr_dAy = .5*(hCG .* m .* g .* (1-PFront))/TWr;
+            dFzf_dAy = 1*(hCG .* m .* PFront)/TWf;
+            dFzr_dAy = 1*(hCG .* m .* (1-PFront))/TWr;
             
             Fz_Wf = (m.*g.* PFront)/2;
             Fz_Wr = (m.*g.* (1-PFront))/2;
@@ -162,7 +167,7 @@ for i = 1:length(dSteer)
                 
                 % Calspan TTC Data usual correction factor - 0.7
                 TM_Fx = TM_Fx .* 0.7;
-                TM_Fy = (1).* TM_Fy .* 0.7;
+                TM_Fy = (-1).* TM_Fy .* 0.7;
 
                 FxTire(p,1) = TM_Fx(p) .* cos(dSteer_AllW(i,p)) - TM_Fy(p) .* sin(dSteer_AllW(i,p));
                 FyTire(p,1) = 1*TM_Fx(p) .* sin(dSteer_AllW(i,p)) + TM_Fy(p) .* cos(dSteer_AllW(i,p));
@@ -182,7 +187,8 @@ for i = 1:length(dSteer)
             itFyTire(:,c) =  FyTire;
             itAyBody(c+1,1) = FyBody/m;
             itAxBody(c+1,1) = FxBody/m;
-            res = itAyBody(c+1) - itAyBody(c);
+            itCAyBody(c+1,1) = itAyBody(c+1,1)/g;
+            res = itCAyBody(c+1) - itCAyBody(c);
     
             % % Tests the weight transfer eqn
             % plot(1:c, itFz);
@@ -217,33 +223,46 @@ for i = 1:length(dSteer)
 
 end % dSteer End
 
-%%
+%% PLOTTING
 
 saveAxVel = zeros(size(saveAyBody));
 saveAyVel = zeros(size(saveAyBody));
+saveCAyVel = zeros(size(saveAyBody));
 
 
 for i = 1:length(SA_CG)
 
     saveAxVel(:,i) = saveAxBody(:,i) .* cos(SA_CG) + saveAyBody(:,i) .* sin(SA_CG);
     saveAyVel(:,i) = saveAyBody(:,i) .* cos(SA_CG) - saveAxBody(:,i) .* sin(SA_CG);
-
+    saveCAyVel(:,i) = saveAyVel(:,i)/g;
 end
 
 figure
 hold on
 grid on
 for i = 1:length(dSteer)
-    steer = plot(saveAyVel(i,:),saveMzBody(i,:), "Color", "blue", 'LineStyle','--');
+    steer = plot(saveCAyVel(i,:),saveMzBody(i,:), "Color", "blue", 'LineStyle','--');
 end
 
 for i = 1:length(SA_CG)
-    slip = plot(saveAyVel(:,i),saveMzBody(:,i), "Color", "red");
+    slip = plot(saveCAyVel(:,i),saveMzBody(:,i), "Color", "red");
 end
 
 legend([steer, slip],{"Constant Steer", "Constant Slip"}, "Location","northeast")
 
-xlabel("C_{(AY)}")
+xlabel("C_{AY}")
 ylabel("C_N")
+xlim([-2,2])
+ylim([-1,1])
 
-title("SA in [-12, 12] & Steering in [-30, 30]")
+if PFront > 0.5
+    resp = 'Understeer' ;
+elseif PFront < 0.5
+    resp = 'Oversteer';
+else
+    resp = 'Neutral Steer';
+end
+
+title( ...
+     {['SA: [' num2str(min(rad2deg(SA_CG))) ',' num2str(max(rad2deg(SA_CG))) '] & Steering: [' num2str(min(rad2deg(dSteer))) ',' num2str(max(rad2deg(dSteer))) ']'], ...
+      ['Expected Response: '  resp]});
