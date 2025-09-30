fdc08eebed0a45d7197c9c7e8a80c5db7eaa9c4c
advay03 <140080682+advay03@users.noreply.github.com>
2025-09-29 17:47:40 -0700

    Update getAbsoluteCAy.m

--- Milliken Moment Method/MMD/+mmd/+ analysis/getAbsoluteCAy.m
+++ Milliken Moment Method/MMD/+mmd/+ analysis/getAbsoluteCAy.m
@@ -1,10 +1,11 @@
-function absoluteCAy = getAbsoluteCAy(result.rawData, result.SA_CG, result.dSteer)
-
+function absoluteCAy = getAbsoluteCAy(result,grid)
+    SA_CG = grid.SA_CG;
+    dSteer = grid.dSteer;
     [SA_CG, dSteer] = meshgrid(SA_CG, dSteer);
-    dataPoints = [rawData.saveCAyVel(1:end);
+    dataPoints = [result.CAyVel(1:end);
                   SA_CG(1:end);
                   dSteer(1:end);
-                  rawData.saveMzBody(1:end)];
+                  result.MzBody(1:end)];
 
     [absoluteCAy.CAy, idx] = max(dataPoints(1, :));
     absoluteCAy.SA = dataPoints(2, idx);
