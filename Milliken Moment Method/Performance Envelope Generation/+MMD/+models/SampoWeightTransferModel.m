9b6642b4f5148f109a6c1744aa228a1bb837b1c6
Jonah Grancell <jsgrancell@ucdavis.edu>
2025-11-11 20:40:37 -0800

    Added code for Sampo Weight Transfer Model

--- Milliken Moment Method/Performance Envelope Generation/+MMD/+models/SampoWeightTransferModel.m
+++ Milliken Moment Method/Performance Envelope Generation/+MMD/+models/SampoWeightTransferModel.m
@@ -1,7 +1,22 @@
-function [outputArg1,outputArg2] = SampoWeightTransferModel(inputArg1,inputArg2)
-%SAMPOWEIGHTTRANSFERMODEL Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+% Function to find weight transfer for the front and rear of a car using
+% the flexible vehicle model. This function takes an input of a structure
+% of car parameters, along with the lateral acceleration (Ay)
+function [deltaFzFront, deltaFzRear] = SampoWeightTransfer(carParams, Ay)
+    kF = carParams.kF;                                                     % For the sake of simplicity, members of carParams structure are assigned
+    kR = carParams.kR;                                                     % to shorter local variables within this function
+    kC = carParams.kC;
+    m_uF = carParams.m_uF;
+    m_uR = carParams.m_uR;
+    TWf = carParams.TWf;
+    TWr = carParams.TWr;
+    h_uF = carParams.h_uF;
+    h_uR = carParams.h_uR;
+    zF = carParams.zF;
+    zR = carParams.zR;
+    d_sF = carParams.h_sF - zF;
+    d_sR = carParams.h_sR - zR;
+    m_sF = carParams.m_s*carParams.b_s/carParams.WB;
+    m_sR = carParams.m_s*carParams.a_s/carParams.WB;
+    deltaFzFront = ((kF*d_sF*m_sF)/(kF+(kR*kC/(kR+kC)))+((kF*kC/(kF+kC))*d_sR*m_sR)/(kF*kC/(kF+kC)+kR)+zF*m_sF+h_uF*m_uF)*Ay/TWf;
+    deltaFzRear = ((kR*kC/(kR+kC)*d_sF*m_sF)/(kF+kR*kC/(kR+kC))+(kR*d_sR*m_sR)/(kF*kC/(kF+kC)+kR)+zR*m_sR+h_uR*m_uR)*Ay/TWr;
 end
-
