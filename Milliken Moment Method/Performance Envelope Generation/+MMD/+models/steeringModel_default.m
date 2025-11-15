a52b937de085194b987a0368ab9416ebf688424a
Qiyuan Tan <119595547+QiyuanTan@users.noreply.github.com>
2025-11-15 13:16:46 -0800

    ggv data

--- Milliken Moment Method/Performance Envelope Generation/+MMD/+models/steeringModel_default.m
+++ Milliken Moment Method/Performance Envelope Generation/+MMD/+models/steeringModel_default.m
@@ -1,7 +1,7 @@
 function dSteer_Allw = steeringModel_default(carParams, dSteer)
-    dSteer_W1 = carParams.toe_f + dSteer;
-    dSteer_W2 = -carParams.toe_f + dSteer;
-    dSteer_W3 = carParams.toe_r + dSteer.*0;
-    dSteer_W4 = -carParams.toe_r + dSteer.*0;
+    dSteer_W1 = -carParams.toe_f + dSteer;
+    dSteer_W2 = carParams.toe_f + dSteer;
+    dSteer_W3 = -carParams.toe_r + dSteer.*0;
+    dSteer_W4 = carParams.toe_r + dSteer.*0;
     dSteer_Allw = [dSteer_W1, dSteer_W2, dSteer_W3, dSteer_W4];
 end
--- Milliken Moment Method/Performance Envelope Generation/+MMD/test/sensitivity test/front_roll_stiffness_sweep.m
+++ Milliken Moment Method/Performance Envelope Generation/+MMD/test/sensitivity test/front_roll_stiffness_sweep.m
@@ -3,14 +3,13 @@ clear; clc; close all;
 fe13params = load("+Cars/fe13SampoParams").fe13params;
 FE13 = Cars.FE13(fe13params);
 
-kR = linspace(200, 2000, 50);
-carParamSweep = repmat(FE13, size(kR));
+ax = linspace(-1, 1, 10);
+carParamSweep = repmat(FE13, size(ax));
 
-for i = 1:length(kR)
-    carParamSweep(i).kR = kR(i);
-end
+% for i = 1:length(ax)
+%     carParamSweep(i).kR = ax(i);
+% end
 
-V = linspace(7, 40, 2);
 %%
 
 models.weightTransferFn = @MMD.models.SampoWeightTransfer;
@@ -18,16 +17,16 @@ grid.SA_CG = -12:0.05:1;
 grid.dSteer = -20:20;
 grid.V = 30;
 
-stability = zeros(size(kR));
-control = zeros(size(kR));
-maxAy = zeros(size(kR));
-SA = zeros(size(kR));
-dS = zeros(size(kR));
+stability = zeros(size(ax));
+control = zeros(size(ax));
+maxAy = zeros(size(ax));
+SA = zeros(size(ax));
+dS = zeros(size(ax));
 
-for i = 1:length(kR)
+for i = 1:length(ax)
     carParams = carParamSweep(i);
     mmd = MMD.MMD(carParams, models);
-    result = mmd.evaluate(grid, "free_rolling");
+    result = mmd.evaluate(grid, "level_surface", ax(i));
     [stability(i), control(i)] = MMD.analysis.getStabilityNControl(result);
     MaxAyResult = MMD.analysis.getSteadyStateCAy(result);
     maxAy(i) = MaxAyResult.CAy;
@@ -41,25 +40,31 @@ end
 tiledlayout(2, 2)
 
 nexttile
-plot(kR, stability)
-title("stability vs rear roll stiffness")
-xlabel("rear roll stiffness (Nm/deg)")
+plot(ax, stability)
+title("stability vs Ax")
+xlabel("Ax (g)")
 ylabel("stability (Nm/deg)")
 
 nexttile
-plot(kR, control)
-title("control vs rear roll stiffness")
-xlabel("rear roll stiffness (Nm/deg)")
+plot(ax, control)
+title("control vs Ax")
+xlabel("Ax (g)")
 ylabel("control (Nm/deg)")
 
 nexttile
-plot(kR, SA)
-title("SA under max SS conering vs rear roll stiffness")
-xlabel("rear roll stiffness (Nm/deg)")
+plot(ax, SA)
+title("SA under max SS conering vs Ax")
+xlabel("Ax (g)")
 ylabel("SA (deg)")
 
 nexttile
-plot(kR, dS)
-title("steering under max SS conering vs rear roll stiffness")
-xlabel("rear roll stiffness (Nm/deg)")
+plot(ax, dS)
+title("steering under max SS conering vs Ax")
+xlabel("Ax (g)")
 ylabel("steer angle (deg)")
+
+figure;
+plot(ax, maxAy)
+title("Ay vs Ax")
+xlabel("Ax (g)")
+ylabel("Ay (g)")
